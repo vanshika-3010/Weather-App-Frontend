bc9cf73ef1234212837b3d3e02d044b6684631b0
Vanshika Mudgal <vanshikamudgal010@gmail.com>
2026-03-06 20:30:29 +0530

    fix vite base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Weather-App-Full-Stack-MERN-/',
   plugins: [react()],
-  base: "/Weather-App-Full-Stack-MERN-/"
 })
